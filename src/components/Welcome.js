ebaf40ccba084494260a2b8b3335bfe4b243b019
Mabaidul-Hoque <mabaidulhoque95@gmail.com>
2023-12-24 18:53:01 +0530

    2nd update

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,8 +6,8 @@ export function Welcome({name}) {
 
   return (
     <>
-      <h1>Hey {name}!</h1>
-      <h2>Welcome to Newton School</h2>
+      <h1>Hey [{name}]!</h1>
+      <h2>Welcome to Newton School.</h2>
     </>
   );
 }
