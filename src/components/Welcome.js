fb89bbc16e8c4986bd792f8adf78cbbbfbf0cc82
Mabaidul-Hoque <mabaidulhoque95@gmail.com>
2023-12-24 18:43:17 +0530

    updated

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,13 @@
 // write code for Welcome component here
+import React from 'react';
+
+export function Welcome({name}) {
+
+
+  return (
+    <>
+      <h1>Hey {name}!</h1>
+      <h2>Welcome to Newton School</h2>
+    </>
+  );
+}
